003b0daabe44c83e5b484c2e4236fd0848420ff2
Somkin Constantine <somkin-k@mail.ru>
2016-04-11 22:27:05 +0300

    yes

--- ContactForm.php
+++ ContactForm.php
@@ -3,7 +3,7 @@ if (array_key_exists('message', $_POST)) {
    $to = 'somkin-k@mail.ru';
    $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
    $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
-   $message = "Имя: ".$_POST['name']."\nEmail: ".$_POST['ContactForm']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['message'];
+   $message = "Имя: ".$_POST['name']."\nEmail: ".$_POST['email']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['message'];
    $headers = 'Content-type: text/plain; charset="utf-8"';
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
