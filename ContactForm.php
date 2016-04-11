38385e3f14dacbec4dd5309ca6de5a2b51355fac
Somkin Constantine <somkin-k@mail.ru>
2016-04-11 20:12:51 +0300

    ye

--- ContactForm.php
+++ ContactForm.php
@@ -1,7 +1,15 @@
 <?php 
-  $text =  nl2br($_POST['theform']);
-  $name =  nl2br($_POST['theform']);
-  echo $text;  
-  echo "";
-  echo $msg; 
+    $name=$_REQUEST['name']; 
+    $email=$_REQUEST['email']; 
+    $message=$_REQUEST['message']; 
+    if (($name=="")||($email=="")||($message=="")) 
+        { 
+        echo "All fields are required, please fill <a href=\"\">the form</a> again."; 
+        } 
+    else{         
+        $from="From: $name<$email>\r\nReturn-path: $email"; 
+        $subject="Message sent using your contact form"; 
+        mail("temihren@gmail.com.com", $subject, $message, $from); 
+        echo "Email sent!"; 
+        }
 ?>
